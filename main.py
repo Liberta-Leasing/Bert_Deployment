313fc4a9d1bc936ecccfe30a4adc7a0255457ad9
SteBaum <stephan.baum@edu.dsti.institute>
2022-06-26 15:19:53 +0200

    main modif

--- main.py
+++ main.py
@@ -72,8 +72,6 @@ def lambda_handler(event, context):
     print(os.listdir("/tmp/tesseract_csv_zip/tmp"))
     print("step 7: listing the files matching /tmp/tesseract_csv_zip/tmp/yolo_output_zip/")
     print(os.listdir("/tmp/tesseract_csv_zip/tmp/yolo_output_zip"))
-    print("step 8: listing the files matching /tmp/tesseract_csv_zip/tmp/yolo_output_zip/tmp/")
-    print(os.listdir("/tmp/tesseract_csv_zip/tmp/yolo_output_zip"))
 
     print("####################\n")  
     print("BERT\n")
